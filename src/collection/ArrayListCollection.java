3102ec1a00ca8f19914b0266ba0bd0ca85edccc2
PavelDoldinn <asdq59asdq@yandex.ru>
2024-01-29 15:07:06 +0500

    add_collection_ArrayListCollection_ex4

--- src/collection/ArrayListCollection.java
+++ src/collection/ArrayListCollection.java
@@ -7,10 +7,28 @@ public class ArrayListCollection {
 
     public static void main(String[] args) {
 
-        ex3();
+        ex4();
 
     }
 
+    public static void ex4() {
+
+        /**
+         * Напишите Java-программу для извлечения элемента (по указанному индексу) из заданного списка массивов.
+         */
+
+        List<Integer> integerList = new ArrayList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(4);
+        integerList.add(5);
+
+        System.out.println(integerList.get(4));
+        System.out.println(integerList.get(0));
+        System.out.printf("%d, %d", integerList.get(0), integerList.get(4));
+
+    }
     public static void ex3() {
 
         List<Integer> integerList = new ArrayList<>();
