1f7fc11f89c9bdc804adcf9a1f0c9bb78731b0da
PavelDoldinn <asdq59asdq@yandex.ru>
2024-02-02 00:23:54 +0500

    add_collection_LinkedListCollection_ex1

--- src/collection/LinkedListCollection.java
+++ src/collection/LinkedListCollection.java
@@ -1,17 +1,28 @@
 package collection;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class LinkedListCollection {
 
     public static void main(String[] args) {
 
-
+        ex1();
 
     }
 
+
     public static void ex1() {
 
+        //Напишите программу на Java, чтобы добавить указанный элемент в конец связанного списка
 
-
+        List<Integer> integerList = new LinkedList<>();
+        integerList.add(1);
+        integerList.add(2);
+        integerList.add(3);
+        integerList.add(4);
+        integerList.addLast(5);
+        System.out.println(integerList);
     }
 
 }
