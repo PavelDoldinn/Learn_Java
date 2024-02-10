debe7007a52c2472930d483f1e573551ffdc5089
PavelDoldinn <asdq59asdq@yandex.ru>
2024-02-10 23:00:14 +0500

    add_algorithms_Bubble_sortBubble2

--- src/algorithms/Bubble.java
+++ src/algorithms/Bubble.java
@@ -7,8 +7,27 @@ public class Bubble {
     public static void main(String[] args) {
 
         int[] nums = new int[]{8, 6, 5, 9, 1, 4};
-        sortBubble1(nums);
+        sortBubble2(nums);
+    }
 
+    public static void sortBubble2(int[] nums) {
+
+        System.out.println(Arrays.toString(nums));
+        int temp;
+        boolean flag = false;
+
+        while (!flag) {
+            flag = true;
+            for (int i = 0; i < nums.length - 1; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    flag = false;
+                    temp = nums[i + 1];
+                    nums[i + 1] = nums[i];
+                    nums[i] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(nums));
     }
 
     public static void sortBubble1(int[] nums) {
