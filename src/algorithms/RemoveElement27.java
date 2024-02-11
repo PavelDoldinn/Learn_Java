92c133045d618e30d8a8ca353e1ed73e39616700
PavelDoldinn <asdq59asdq@yandex.ru>
2024-02-11 14:49:31 +0500

    add_algorithms_RemoveElement27_removeElement1

---
+++ src/algorithms/RemoveElement27.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RemoveElement27 {
+
+    public static void main(String[] args) {
+
+        // Есть массив nums и число k, удалите из масива числа равные k
+        // Вернуть массив без k
+
+        int[] nums = new int[]{3, 2, 3, 1, 3, 2};
+        int target = 3;
+
+        System.out.println(removeElement1(nums, target));
+
+    }
+    public static List removeElement1(int[] nums, int target) {
+
+        List<Integer> integerList = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != target) {
+                integerList.add(nums[i]);
+            }
+        }
+        return integerList;
+    }
+
+}
